a4aa38de4ad133d7b409227eca60692d738f513a
Fatih Fawwaz <fatihfwz87@gmail.com>
2024-09-16 20:45:42 +0700

    API Routes

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,10 +1,18 @@
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
+
+type productType = {
+  id: number;
+  name: string;
+  price: number;
+  size: string;
+};
 
 const ProductPage = () => {
   // Kalau misalkan belum login
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
   const { push } = useRouter();
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     if (!isLogin) {
@@ -13,10 +21,21 @@ const ProductPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    fetch('api/products')
+      .then((res) => res.json())
+      .then((response) => {
+        setProducts(response.data);
+      });
+  }, []);
+
   return (
     <>
       <div>
         <h1>Product page</h1>
+        {products.map((product: productType) => (
+          <p key={product.id}>{product.name}</p>
+        ))}
       </div>
     </>
   );
