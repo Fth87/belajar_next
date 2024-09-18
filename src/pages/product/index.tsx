008f55ef700987a08c355edd5937c521a4add684
Fatih Fawwaz <fatihfwz87@gmail.com>
2024-09-18 09:01:52 +0700

    Client side rendering

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,18 +1,13 @@
+import ProductView from '@/views/product/Index';
 import { useRouter } from 'next/router';
 import React, { use, useEffect, useState } from 'react';
-
-type productType = {
-  id: number;
-  name: string;
-  price: number;
-  size: string;
-};
-
+import useSWR from 'swr';
+import { fetcher } from '@/lib/swr/fetcher';
 const ProductPage = () => {
   // Kalau misalkan belum login
   const [isLogin, setIsLogin] = useState(true);
   const { push } = useRouter();
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
 
   useEffect(() => {
     if (!isLogin) {
@@ -21,22 +16,20 @@ const ProductPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  useEffect(() => {
-    fetch('api/products')
-      .then((res) => res.json())
-      .then((response) => {
-        setProducts(response.data);
-      });
-  }, []);
+  const { data, error, isLoading } = useSWR('/api/products', fetcher);
+  console.log(data);
+
+  // useEffect(() => {
+  //   fetch('api/products')
+  //     .then((res) => res.json())
+  //     .then((response) => {
+  //       setProducts(response.data);
+  //     });
+  // }, []);
 
   return (
     <>
-      <div>
-        <h1>Product page</h1>
-        {products.map((product: productType) => (
-          <p key={product.id}>{product.name}</p>
-        ))}
-      </div>
+      <ProductView products={isLoading ? [] : data.data} />
     </>
   );
 };
